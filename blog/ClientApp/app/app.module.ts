9b476214764cdbf850d1f1d300a3d8f877a79bc4
jth0mass0n <jonnerin@gmail.com>
2018-12-09 08:40:51 -0800

    cleaning up code a bit.

--- blog/ClientApp/app/app.module.ts
+++ blog/ClientApp/app/app.module.ts
@@ -4,9 +4,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { FooterComponent } from "./shared/layout/footer/footer.component";
 import { HeaderComponent } from "./shared/layout/header/header.component";
-
 import { AppRoutingModule } from './app-routing.module';
-
 import { FormsModule } from "@angular/forms";
 import { APP_BASE_HREF } from '@angular/common';
 import { AboutComponent } from './home/about/about.component';
