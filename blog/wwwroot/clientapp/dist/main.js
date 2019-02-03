6f98459217370520e840b61daa32792aeab778b5
jth0mass0n <jonnerin@gmail.com>
2019-02-03 09:54:59 -0800

    adding comments and styling

--- blog/wwwroot/clientapp/dist/main.js
+++ blog/wwwroot/clientapp/dist/main.js
@@ -348,7 +348,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\jthomasson\source\repos\JonThomasson.Blog\blog\ClientApp\main.ts */"./main.ts");
+module.exports = __webpack_require__(/*! C:\Users\Stache\source\repos\jonthomasson\JonThomasson.Blog\blog\ClientApp\main.ts */"./main.ts");
 
 
 /***/ })
